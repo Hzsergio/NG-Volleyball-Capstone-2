7dcd007c12fa2c9a16c19d8512d40880e43819aa
Jonah Burke <burke099@csusm.edu>
2024-04-16 09:50:04 -0700

    messaging

--- Ladder/backend/messaging/urls.py
+++ Ladder/backend/messaging/urls.py
@@ -1,17 +1,11 @@
 from django.urls import path
-from . import views
-from django.urls import path
-from .views import *
 from rest_framework.routers import DefaultRouter
-
-app_name = 'messaging'
-
-urlpatterns = [
-    path('inbox', views.inbox_view, name='inbox'),
-    path('compose/', views.compose_message_view, name='compose'),
-    path('message/<int:message_id>/', views.message_detail_view, name='message_detail'),
-]
-
+from .views import MessageViewSet, Messages
+'''
 router = DefaultRouter()
-router.register('inbox', views.inbox_view, basename='inbox')
+router.register('messages', MessageViewSet, basename='Messages')
+'''
+router = DefaultRouter()
+router.register('messages', Messages, basename='Messages')
+
 urlpatterns = router.urls
