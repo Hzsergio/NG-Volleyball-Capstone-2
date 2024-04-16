3db6d9088abf33147cd819959247babda0f9b801
Blake F <fullerton199@gmail.com>
2024-04-16 09:32:05 -0700

    messaging updates

--- Ladder/backend/messaging/models.py
+++ Ladder/backend/messaging/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
+from users.models import User
+
 
 class Message(models.Model):
-    sender = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='sent_messages', on_delete=models.CASCADE)
-    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='received_messages', on_delete=models.CASCADE)
+    
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    recipient = models.ForeignKey(User,related_name='received_messages', on_delete=models.CASCADE)
     content = models.TextField()
     sent_time = models.DateTimeField(default=timezone.now)
     read = models.BooleanField(default=False)
+    #captain = models.ForeignKey(User,related_name='team_captain',on_delete=models.CASCADE)
+
 
     def __str__(self):
         return f"From {self.sender} to {self.recipient} - {self.sent_time}"
--- Ladder/backend/messaging/serializers.py
+++ Ladder/backend/messaging/serializers.py
@@ -2,6 +2,27 @@ from rest_framework import serializers
 from .models import *
 
 class MessageSerializer(serializers.ModelSerializer):
+    sender_username = serializers.ReadOnlyField(source='sender.username')
+    receiver_username = serializers.ReadOnlyField(source='recipient.username')
+
     class Meta:
         model = Message
-        fields = ['id', 'sender', 'recipient', 'content', 'sent_time', 'read']
+        fields = ['id', 'sender', 'recipient', 'content', 'sent_time', 'read', 'sender_username', 'receiver_username']
+
+
+'''
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender_username = serializers.SerializerMethodField()
+    receiver_username = serializers.SerializerMethodField()
+    class Meta:
+        model = Message
+        fields = ['id', 'sender', 'recipient', 'content', 'sent_time', 'read','sender_username','receiver_username']
+    
+    def get_sender_username(self, obj):
+            return obj.sender.username
+    
+    def get_receiver_username(self, obj):
+            return obj.receiver.username
+
+    '''
