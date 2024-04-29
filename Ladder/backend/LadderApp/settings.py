bab12825faa2b52ddda7b0f51cf2164f0d61bd62
Hzsergio <123702221+Hzsergio@users.noreply.github.com>
2024-04-28 22:34:17 -0700

    Update settings.py

--- Ladder/backend/LadderApp/settings.py
+++ Ladder/backend/LadderApp/settings.py
@@ -89,23 +89,23 @@ WSGI_APPLICATION = 'LadderApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'LadderDB',
-#         'USER': 'postgres',
-#         'PASSWORD': 'password',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Ladder_DB',
+        'USER': 'postgres',
+        'PASSWORD': 'Spring2024',
+        'HOST': 'ladderdb.clboclk3becx.us-west-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
