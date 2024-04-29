1f0ae38f1679019a2fcc3ab8216e901139f45246
Hzsergio <123702221+Hzsergio@users.noreply.github.com>
2024-04-28 22:57:38 -0700

    Update settings.py

--- Ladder/backend/LadderApp/settings.py
+++ Ladder/backend/LadderApp/settings.py
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = ['18.208.190.100']
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "https://prod.d2y0ii0xxyejop.amplifyapp.com/"
 
 ]
 # Application definition
